2822c67194d5b6ba4799993a17abfa36bab9bb66
Austine <chucky891213@naver.com>
2024-07-31 10:17:19 +0900

    24-07-31 project setting

---
+++ src/test/test01.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
