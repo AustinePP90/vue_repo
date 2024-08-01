c3a8c0486fe0cc4b61a4a34cdb506f875ab0ca4f
Austine <chucky891213@naver.com>
2024-08-01 10:44:34 +0900

    24-08-01 TestView파일에서 Vue연습

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/test">Test</router-link>
-    <router-link to="/basic">3.컴포넌트(기본)</router-link>
+    <router-link to="/basic">3.컴포넌트(BASIC)</router-link>
   </nav>
   <router-view />
 </template>
