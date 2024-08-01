688e1ddb3e1d507ccc835ec01214c22e6bc916cd
Austine <chucky891213@naver.com>
2024-08-01 16:09:56 +0900

    24-08-01 TestView파일, 추가 연습

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -18,6 +18,7 @@
       <input type="text" v-model="text" />
     </div>
     <div><textarea v-model="text"></textarea></div>
+    <br /><br /><br />
     <!-- ------------------------------------------------------------ -->
     <table>
       <thead>
@@ -33,6 +34,7 @@
         </tr>
       </tbody>
     </table>
+    <br /><br /><br />
     <!-- ------------------------------------------------------------ -->
     <div>
       <!-- v-model.number, 입력값을 자동으로 숫자로 변환함, parseFloat()를 사용한 것과 유사함 -->
@@ -45,6 +47,7 @@
       <p>평균: {{ average.toFixed(2) }}</p>
       <p>학점: {{ grade }}</p>
     </div>
+    <br /><br /><br />
     <!-- ------------------------------------------------------------ -->
     <label><input type="number" v-model.number="number" />단</label>
     <button @click="calculate">계산</button>
@@ -53,6 +56,39 @@
       <div v-for="multiNum in multiNums" :key="multiNum">
         <p>{{ number }} * {{ multiNum }} = {{ results[multiNum - 1] }}</p>
       </div>
+    </div>
+    <br /><br /><br />
+    <!-- ------------------------------------------------------------ -->
+    <div>
+      <p>
+        <label
+          >이메일<input
+            type="text"
+            placeholder="이메일을 입력하세요"
+            v-model="emailValue"
+            @input="changeEmail" /></label
+        ><span>{{ msgEmail }}</span>
+      </p>
+      <p>
+        <label
+          >비밀번호<input
+            type="text"
+            @input="changePwd"
+            v-model="passwordValue"
+            placeholder="비밀번호를 입력하세요"
+        /></label>
+        <span>{{ msgPwd }}</span>
+      </p>
+      <p>
+        <label
+          >비밀번호확인<input
+            type="text"
+            @input="changePwd2"
+            v-model="passwordValueConfirm"
+            placeholder="비밀번호확인을 입력하세요"
+        /></label>
+        <span>{{ msgPwd2 }}</span>
+      </p>
     </div>
   </div>
 </template>
@@ -67,22 +103,29 @@ export default {
     return {
       title: "2ne1 <b>15주년</b> 콘서트",
       text: "",
-
+      // ---------------------------------------------------------------------------
       ygs: [
         { name: "CL", age: 19, position: "리더" },
         { name: "공민지", age: 16, position: "막내" },
         { name: "산다라", age: 26, position: "비주얼" },
         { name: "박봄", age: 26, position: "메인보컬" },
       ],
-
+      // ---------------------------------------------------------------------------
       kor: "",
       eng: "",
       math: "",
-
+      // ---------------------------------------------------------------------------
       number: 1,
       multiNums: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       results: [],
       showResults: false,
+      // ---------------------------------------------------------------------------
+      emailValue: "",
+      msgEmail: "이메일을 입력하세요",
+      passwordValue: "",
+      msgPwd: "비밀번호를 입력하세요",
+      passwordValueConfirm: "",
+      msgPwd2: "비밀번호를 다시 입력하세요",
     };
   },
 
@@ -103,6 +146,38 @@ export default {
     calculate() {
       this.results = this.multiNums.map((num) => this.number * num);
       this.showResults = true;
+    },
+    // ---------------------------------------------------------------------------
+    changeEmail() {
+      // this.emailValue이 이메일 형식에 맞는지 체크
+      const emailExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 문자열 + @ + 문자열 + . + 문자열
+      if (this.emailValue.length == 0) {
+        this.msgEmail = "이메일을 입력하세요";
+      } else if (emailExp.test(this.emailValue) == true) {
+        this.msgEmail = "정상적인 이메일 주소입니다.";
+      } else {
+        this.msgEmail = "이메일 주소가 올바르지 않습니다.";
+      }
+    },
+
+    changePwd() {
+      const passwordExp =
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/; // 문자(영어, 대문자 가능)와 숫자 그리고 특수기호로 이뤄진 8자리 비밀번호
+      if (this.passwordValue.length == 0) {
+        this.msgPwd = "비밀번호 8자리를 입력하세요";
+      } else if (passwordExp.test(this.passwordValue) == true) {
+        this.msgPwd = "사용할수 있는 비밀번호 입니다.";
+      } else {
+        this.msgPwd = "사용할수 없는 비밀번호 입니다.";
+      }
+    },
+
+    changePwd2() {
+      if (this.passwordValueConfirm == this.passwordValue) {
+        this.msgPwd2 = "비밀번호를 제대로 입력했습니다.";
+      } else {
+        this.msgPwd2 = "비밀번호를 제대로 입력하세요.";
+      }
     },
   },
 };
