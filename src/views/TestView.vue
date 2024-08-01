0164905cc14c1daf25ae220c58f133675561cdbf
Austine <chucky891213@naver.com>
2024-08-01 14:06:54 +0900

    24-08-01 TestView파일, 추가 연습

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -18,20 +18,93 @@
       <input type="text" v-model="text" />
     </div>
     <div><textarea v-model="text"></textarea></div>
+    <!-- ------------------------------------------------------------ -->
+    <table>
+      <thead>
+        <th>이름</th>
+        <th>나이</th>
+        <th>포지션</th>
+      </thead>
+      <tbody>
+        <tr v-for="(yg, index) in ygs" v-bind:key="index">
+          <td>{{ yg.name }}</td>
+          <td>{{ yg.age }}</td>
+          <td>{{ yg.position }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- ------------------------------------------------------------ -->
+    <div>
+      <!-- v-model.number, 입력값을 자동으로 숫자로 변환함, parseFloat()를 사용한 것과 유사함 -->
+      <label>국어: <input type="number" v-model.number="kor" /></label>
+      <br />
+      <label>영어: <input type="number" v-model.number="eng" /></label>
+      <br />
+      <label>수학: <input type="number" v-model.number="math" /></label>
+      <br />
+      <p>평균: {{ average.toFixed(2) }}</p>
+      <p>학점: {{ grade }}</p>
+    </div>
+    <!-- ------------------------------------------------------------ -->
+    <label><input type="number" v-model.number="number" />단</label>
+    <button @click="calculate">계산</button>
+    <p>{{ number }}단</p>
+    <div v-if="showResults">
+      <div v-for="multiNum in multiNums" :key="multiNum">
+        <p>{{ number }} * {{ multiNum }} = {{ results[multiNum - 1] }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TestView",
+
   components: {}, // 자식 컴포넌트들을 말한다
+
   data() {
     return {
       title: "2ne1 <b>15주년</b> 콘서트",
       text: "",
+
+      ygs: [
+        { name: "CL", age: 19, position: "리더" },
+        { name: "공민지", age: 16, position: "막내" },
+        { name: "산다라", age: 26, position: "비주얼" },
+        { name: "박봄", age: 26, position: "메인보컬" },
+      ],
+
+      kor: "",
+      eng: "",
+      math: "",
+
+      number: 1,
+      multiNums: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      results: [],
+      showResults: false,
     };
   },
-  methods: {},
+
+  computed: {
+    average() {
+      return (this.kor + this.eng + this.math) / 3;
+    },
+    grade() {
+      if (this.average >= 90) return "A";
+      if (this.average >= 80) return "B";
+      if (this.average >= 70) return "C";
+      if (this.average >= 60) return "D";
+      return "F";
+    },
+  },
+
+  methods: {
+    calculate() {
+      this.results = this.multiNums.map((num) => this.number * num);
+      this.showResults = true;
+    },
+  },
 };
 </script>
 
